dabed8e2060c6412535eeb4afcdddd69ca1a6a48
Parvez8bd <parvez8bd@gmail.com>
2021-05-26 15:12:17 +0600

    fist work

---
+++ index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("extra-menu").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("extra-menu").style.width = "0";
+  }
